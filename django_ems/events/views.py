5c56cb86dc7c7ce1910319342ca4612f464239fc
TEJASWINI D S <tejashwinidevikoppa@gmail.com>
2024-10-16 23:14:25 -0400

    push

--- django_ems/events/views.py
+++ django_ems/events/views.py
@@ -1,5 +1,3 @@
-# Create your views here.
-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Event, Registration
